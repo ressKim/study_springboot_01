11fb70087e90c19f2a4f5d1e95081f944136328a
hyukjunK <sh21dlat@gmail.com>
2020-10-20 01:12:25 +0900

    html template add

--- src/main/java/com/studyspringboot03/demo/domain/User.java
+++ src/main/java/com/studyspringboot03/demo/domain/User.java
@@ -1,8 +1,20 @@
-package com.studyspringboot03.demo.web;
+package com.studyspringboot03.demo.domain;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    @Column(nullable = false, length = 20)
     private String userId;
+    
     private String password;
     private String name;
     private String email;
--- src/main/java/com/studyspringboot03/demo/web/UserController.java
+++ src/main/java/com/studyspringboot03/demo/web/UserController.java
@@ -1,5 +1,8 @@
 package com.studyspringboot03.demo.web;
 
+import com.studyspringboot03.demo.domain.User;
+import com.studyspringboot03.demo.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,19 +14,21 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    private List<User> users = new ArrayList<User>();
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping("/create")
+
+    @PostMapping("/user/create")
     public String create(User user) {
         System.out.println("user : " + user);
-        users.add(user);
-        return "redirect:/list";
+        userRepository.save(user);
+        return "redirect:/user/list";
     }
 
-    @GetMapping("/list")
+    @GetMapping("/user/list")
     public String list(Model model){
 
-        model.addAttribute("users", users);
+        model.addAttribute("users", userRepository.findAll());
 
         return "list";}
 
